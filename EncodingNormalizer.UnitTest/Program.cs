ec019d8445fb33073fce5600b71ff13cad3f03e7
lindexi <lindexi@cvte.com>
2017-02-07 17:18:49 +0800

    添加单元测试

---
+++ EncodingNormalizer.UnitTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EncodingNormalizer.UnitTest
+{
+    [TestClass]
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [TestMethod]
+        public void MainTest()
+        {
+            string[] arg = new[]
+              {
+                "-f",
+                "E:\\程序\\ethylene156\\EncodingNormalior"
+            };
+
+            EncodingNormalior.Program.Main(arg);
+        }
+
+        [TestMethod]
+        public void NoFolderArgTest()
+        {
+            //没有存在文件夹
+            string[] arg = new[]
+            {
+                "-f",
+                //"E:\\程序\\ethylene156\\EncodingNormalior"
+            };
+
+            try
+            {
+                EncodingNormalior.Program.Main(arg);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.GetType(), typeof(ArgumentException));
+            }
+        }
+    }
+}
