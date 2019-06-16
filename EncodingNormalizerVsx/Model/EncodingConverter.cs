9f2a0bfb811bd0b91676424843f6a660e907b760
lindexi <lindexi_gd@163.com>
2019-06-16 12:08:03 +0800

    文件编码转换
    #15

---
+++ EncodingNormalizerVsx/Model/EncodingConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EncodingNormalizerVsx.Model
+{
+    public class EncodingConverter : IEncodingConverter
+    {
+        /// <inheritdoc />
+        public EncodingConverter(Encoding encoding)
+        {
+            Encoding = encoding;
+        }
+
+        public string Read(FileInfo file)
+        {
+            using (var stream = new StreamReader(file.OpenRead(), Encoding))
+            {
+                return stream.ReadToEnd();
+            }
+        }
+
+        public void Storage(string str, FileInfo file)
+        {
+            using (var stream = new StreamWriter(file.Open(FileMode.Create, FileAccess.Write), Encoding))
+            {
+                stream.Write(str);
+            }
+        }
+
+        public Encoding Encoding { get; }
+
+        public virtual string EncodingName => Encoding.EncodingName;
+    }
+}
---
+++ EncodingNormalizerVsx/Model/Utf8WithoutBomEncodingConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace EncodingNormalizerVsx.Model
+{
+    public class Utf8WithoutBomEncodingConverter : EncodingConverter, IEncodingConverter
+    {
+        /// <inheritdoc />
+        public Utf8WithoutBomEncodingConverter() : base(new UTF8Encoding(false))
+        {
+        }
+
+        public override string EncodingName { get; } = "Utf8 without Bom";
+    }
+}
