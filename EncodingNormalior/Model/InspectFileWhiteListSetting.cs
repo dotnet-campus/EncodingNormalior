7ffb5619bda7ef95b681b8aaa61571ec314270f4
lindexi <lindexi_gd@163.com>
2020-10-17 15:41:48 +0800

    修复构建

--- EncodingNormalior/Model/InspectFileWhiteListSetting.cs
+++ EncodingNormalior/Model/InspectFileWhiteListSetting.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using EncodingNormalior.Annotations;
-using EncodingNormalior.Resource;
 
 namespace EncodingNormalior.Model
 {
