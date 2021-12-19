193f3fb540d9d586f16f0b948a1c664879538be1
lindexi <lindexi_gd@163.com>
2021-12-19 11:25:09 +0800

    升级版本

--- EncodingNormalizerVsix/Properties/AssemblyInfo.cs
+++ EncodingNormalizerVsix/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EncodingNormalizerVsix")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Encoding tool can check all files in solution and normalize the encoding")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dotnet campus")]
 [assembly: AssemblyProduct("EncodingNormalizerVsix")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright (c) 2020-2021 dotnet-campus")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
